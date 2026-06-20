4d511170f94c1f64504c181d690f05e3fd696bda
李狗剩 <141636164+upwardlmy@users.noreply.github.com>
2026-06-20 20:15:35 +0800

    Delete frontend directory

--- frontend/src/types/dictionary.ts
+++
@@ -1,4 +0,0 @@
-export interface DictionaryItem {
-  value: string;
-  label: string;
-}
--- frontend/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./api";
-export * from "./user";
-export * from "./project";
-export * from "./notification";
-export * from "./dictionary";
